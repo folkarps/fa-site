ddca5f617311cc24c2884154b85b4dc2443923c7
Ben Hussey <ben@blip2.net>
2021-11-17 22:16:22 +0000

    added forum redirect

---
+++ pages/forum/_slug.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <v-container style="max-width: 800px; padding-bottom: 50px">
+      <h1>The forums have moved</h1>
+      <p>Please update your bookmarks, you will redirected in a few seconds.</p>
+      <p><a href="http://forum.folkarps.com">Click Here</a> if you are not redirected.</p>
+    </v-container>
+    <meta http-equiv="refresh" content="3; URL=http://forum.folkarps.com" />
+  </div>
+</template>
